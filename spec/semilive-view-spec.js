50914958ccbd86700329cc57892c23a9fe6611b7
agraebe <agraebe@uber.com>
2017-05-20 10:12:34 -0700

    generated package

---
+++ spec/semilive-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SemiliveView from '../lib/semilive-view';
+
+describe('SemiliveView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
